185c0802d7ae65879ede930b4793f2dd1f1d3ab4
gipsoho <isaie.debeze@etu.unistra.fr>
2024-01-09 23:33:57 +0100

    new function && fix the lama model verification

--- lib/ia-stucieux.py
+++ lib/ia-stucieux.py
@@ -6,14 +6,15 @@ import io
 import base64
 from PIL import Image
 
+
 class ia_stucieux:
     def __init__(self, ip="127.0.0.1", port=7860, img_path="./img/"):
-        self.max_token = 32
+        self.max_token = 1024
         self.path_model_lama = ""
         self.ip_automatic = ip
         self.port_automatic = port
         self.img_path = img_path
-        self.test_model_lama()
+        self.model_exist()
         self.init_img_folder()
         self.llama = Llama(self.path_model_lama)
 
@@ -64,15 +65,14 @@ class ia_stucieux:
             print("error automatic 1111 api is not running")
             exit(1)
 
-    def test_model_lama(self):
-        if os.listdir('./models/') == []:
+    def model_exist(self):
+        listdir = os.listdir('./models/')
+        if '.gitkeep' in listdir:
+            listdir.remove('.gitkeep')
+        if listdir == []:
             print("No model found in ./models/")
             exit(1)
-            # verif if file is gitkeep
-        if os.listdir('./models/')[0] == '.gitkeep':
-            self.path_model_lama = './models/' + os.listdir('./models/')[1]
-        else:
-            self.path_model_lama = './models/' + os.listdir('./models/')[0]
+        self.path_model_lama = './models/' + listdir[0]
 
     def init_img_folder(self):
         if not os.path.exists('./img'):
@@ -89,9 +89,7 @@ class ia_stucieux:
         prompt = "Generate only a random prompt description for a ai image generator"
         output = self.llama(prompt, max_tokens=self.max_token)
         img = output["choices"][0]["text"]
-        print(img)
         self.generateImg(img)
-
 
     def generateImg(self, prompt):
         # Effectuer la requête HTTP pour convertir le résultat en image
@@ -105,8 +103,18 @@ class ia_stucieux:
         image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
         image.save(f'{self.img_path}/output.png')
 
+    def generate_story(self, theme=None):
+        prompt = "Generate a story in few lines" + (f" with the theme is : {theme}" if theme is not None else "")
+        output = self.llama(prompt, max_tokens=self.max_token)
+        story = output["choices"][0]["text"]
+        print("story : " + story)
+        prompt = f"With this story : {story}, generate a description image to illustrate it"
+        print("prompt : " + prompt)
+        self.generateImg(prompt)
+        return story
+
 
 if __name__ == '__main__':
     print("This is a module, not a script")
     ia = ia_stucieux()
-    ia.generateRandomImg("./img/")
+    ia.generate_story("love")
